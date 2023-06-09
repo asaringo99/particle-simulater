f8c86b79788f064788b648f348cedcef8bcff133
asaringo99 <ka99code@gmail.com>
2023-06-09 10:50:26 +0900

    [fix] factory

--- particle/factory.js
+++ particle/factory.js
@@ -1,13 +1,21 @@
 class ParticleFactory {
     static createRequire(particle){
-        const positionX = particle.positionX
-        const positionY = particle.positionY
-        const velocityX = particle.velocityX
-        const velocityY = particle.velocityY
+        const positionX = particle.position.x
+        const positionY = particle.position.y
+        const velocityX = particle.velocity.x
+        const velocityY = particle.velocity.y
         const radius = particle.radius
         const dencity = particle.dencity
         const mass = radius * radius * dencity
         const buffer = particle.buffer
         return new Particle(positionX, positionY, velocityX, velocityY, radius, mass, buffer)
+    }
+    static createDefaultParticle(){
+        const particle = defaultParticle
+        return this.createRequire(particle)
+    }
+    static createSpecialParticle(){
+        const particle = specialParticle
+        return this.createRequire(particle)
     }
 }
