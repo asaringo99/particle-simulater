a7b1fc4490afb17d4ce3bec2c8253f339f623021
asaringo99 <ka99code@gmail.com>
2023-06-09 10:50:17 +0900

    [fix] config

--- particle/config.js
+++ particle/config.js
@@ -1,5 +1,24 @@
+const viscocity = 0.9
+const defaultParticleSize = 1
+const specialParticleSize = 200
 const mouseEffectDistance = 500;
 const mouseEffectCoefficient = 0.02
+
+const defaultParticle = {
+    position: {x: 1, y: 1},
+    velocity: {x: 1, y: 1},
+    dencity: 0.1,
+    radius: 1,
+    buffer: 1 * 0.5,
+}
+
+const specialParticle = {
+    position: {x: 0.5, y: 0.5},
+    velocity: {x: 0, y: 0},
+    dencity: 1000,
+    radius: 200,
+    buffer: 200 * 0.5,
+}
 
 function canEffectForce(factorPositionX, factorPositionY, nowX, nowY){
     const diffX = (factorPositionX - nowX);
